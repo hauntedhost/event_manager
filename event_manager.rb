1a7713137101d11ac5fba083a3bfab1ca0662ab3
sean <somlor@gmail.com>
2013-01-17 04:03:38 -0600

    add method to clean and print numbers.

--- event_manager.rb
+++ event_manager.rb
@@ -14,8 +14,31 @@ class EventManager
   		puts "#{line[:first_name]} #{line[:last_name]}"
   	end
   end
+
+  def print_numbers
+  	@file.each do |line|
+  		number = clean_number(line[:homephone])
+  		puts number
+  	end
+  end
+
+  def clean_number(number)
+  	junk = "0000000000"
+  	number = number.scan(/\d/).join
+
+  	number = case number.length
+  	when 10 then 
+  		number
+  	when 11
+  		number[0] == "1" ? number[1..-1] : junk
+  	else
+  		junk
+  	end
+  end
+
 end
 
 # Script
 manager = EventManager.new
-manager.print_names
+#manager.print_names
+manager.print_numbers
